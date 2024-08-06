9c63eb2a568f80b59194e3b5e037b46111c36bbe
James McClung <james@themcclungs.net>
2024-08-06 16:54:45 -0400

    output; byte_writer: +write_prelude

--- src/output.rs
+++ src/output.rs
@@ -2,6 +2,11 @@ use std::io::{Result, Write};
 
 mod byte_writer;
 
+pub const FLOAT_64: u8 = 1;
+
+pub const ID_FLOAT: u8 = 1;
+
 pub trait Writer<T> {
+    fn write_prelude<W: Write>(writer: &mut W) -> Result<usize>;
     fn write<W: Write>(writer: &mut W, item: &T) -> Result<usize>;
 }
--- src/output/byte_writer.rs
+++ src/output/byte_writer.rs
@@ -8,6 +8,10 @@ impl Writer<f64> for ByteWriter {
     fn write<W: Write>(writer: &mut W, item: &f64) -> Result<usize> {
         writer.write(&item.to_le_bytes())
     }
+
+    fn write_prelude<W: Write>(writer: &mut W) -> Result<usize> {
+        writer.write(&[FLOAT_64, ID_FLOAT])
+    }
 }
 
 #[cfg(test)]
@@ -17,7 +21,8 @@ mod tests {
     #[test]
     fn write_f64() {
         let mut data = Vec::new();
+        ByteWriter::write_prelude(&mut data).unwrap();
         ByteWriter::write(&mut data, &1.5_f64).unwrap();
-        assert_eq!(data, vec![0, 0, 0, 0, 0, 0, 0xf8, 0x3f]);
+        assert_eq!(data, vec![FLOAT_64, ID_FLOAT, 0, 0, 0, 0, 0, 0, 0xf8, 0x3f]);
     }
 }
