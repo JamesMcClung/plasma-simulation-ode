25f18faaf01328a4b3aa4f35a517275a975d6426
James McClung <james@themcclungs.net>
2024-08-06 16:59:32 -0400

    byte_writer; output: impl write f32

--- src/output.rs
+++ src/output.rs
@@ -2,6 +2,7 @@ use std::io::{Result, Write};
 
 mod byte_writer;
 
+pub const FLOAT_32: u8 = 0;
 pub const FLOAT_64: u8 = 1;
 
 pub const ID_FLOAT: u8 = 1;
--- src/output/byte_writer.rs
+++ src/output/byte_writer.rs
@@ -4,6 +4,16 @@ use super::*;
 
 pub struct ByteWriter;
 
+impl Writer<f32> for ByteWriter {
+    fn write<W: Write>(writer: &mut W, item: &f32) -> Result<usize> {
+        writer.write(&item.to_le_bytes())
+    }
+
+    fn write_prelude<W: Write>(writer: &mut W) -> Result<usize> {
+        writer.write(&[FLOAT_32, ID_FLOAT])
+    }
+}
+
 impl Writer<f64> for ByteWriter {
     fn write<W: Write>(writer: &mut W, item: &f64) -> Result<usize> {
         writer.write(&item.to_le_bytes())
@@ -19,9 +29,17 @@ mod tests {
     use super::*;
 
     #[test]
+    fn write_f32() {
+        let mut data = Vec::new();
+        <ByteWriter as Writer<f32>>::write_prelude(&mut data).unwrap();
+        ByteWriter::write(&mut data, &1.5_f32).unwrap();
+        assert_eq!(data, vec![FLOAT_32, ID_FLOAT, 0, 0, 0xc0, 0x3f]);
+    }
+
+    #[test]
     fn write_f64() {
         let mut data = Vec::new();
-        ByteWriter::write_prelude(&mut data).unwrap();
+        <ByteWriter as Writer<f64>>::write_prelude(&mut data).unwrap();
         ByteWriter::write(&mut data, &1.5_f64).unwrap();
         assert_eq!(data, vec![FLOAT_64, ID_FLOAT, 0, 0, 0, 0, 0, 0, 0xf8, 0x3f]);
     }
