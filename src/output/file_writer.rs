44bac22558c001c7a5ba0c180bfda29bd09f1c33
James McClung <james@themcclungs.net>
2024-08-09 12:35:41 -0400

    file_writer: make generic over F
    for dependency injection later

--- src/output/file_writer.rs
+++ src/output/file_writer.rs
@@ -1,36 +1,38 @@
 use std::{
     fs::File,
-    io::{BufWriter, Result},
+    io::{BufWriter, Result, Write},
     path::PathBuf,
 };
 
 use super::*;
 
-pub struct FileWriter<const BYTES_PER_WORD: u8 = { std::mem::size_of::<Float>() as u8 }> {
-    file: BufWriter<File>,
+pub struct FileWriter<F: Write = File, const BYTES_PER_WORD: u8 = { std::mem::size_of::<Float>() as u8 }> {
+    file: BufWriter<F>,
 }
 
-impl<const BYTES_PER_WORD: u8> FileWriter<BYTES_PER_WORD> {
-    /// Opens a (buffered) file and immediately writes the prelude.
-    pub fn create(path: impl Into<PathBuf>) -> Result<Self> {
-        let file = BufWriter::new(File::create(path.into())?);
-        let mut res = Self { file };
-        res.write_prelude()?;
-        Ok(res)
-    }
-
+impl<F: Write, const BYTES_PER_WORD: u8> FileWriter<F, BYTES_PER_WORD> {
     fn write_prelude(&mut self) -> Result<usize> {
         self.file.write(&[FORMAT_VERSION_MAJOR, FORMAT_VERSION_MINOR, FORMAT_VERSION_PATCH, BYTES_PER_WORD])
     }
 
     pub fn write_bytes<T: TypeID>(&mut self, item: &T) -> Result<usize>
     where
-        BufWriter<File>: WriteBytes<T>,
+        BufWriter<F>: WriteBytes<T>,
     {
         self.file.write_bytes::<BYTES_PER_WORD>(item)
     }
 
     pub fn flush(&mut self) -> Result<()> {
         self.file.flush()
+    }
+}
+
+impl<const BYTES_PER_WORD: u8> FileWriter<File, BYTES_PER_WORD> {
+    /// Opens a (buffered) file and immediately writes the prelude.
+    pub fn create(path: impl Into<PathBuf>) -> Result<Self> {
+        let file = BufWriter::new(File::create(path.into())?);
+        let mut res = Self { file };
+        res.write_prelude()?;
+        Ok(res)
     }
 }
