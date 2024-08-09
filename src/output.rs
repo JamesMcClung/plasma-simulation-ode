e35ec5f77eb6d0f67a5f17a70b0d1008563bb796
James McClung <james@themcclungs.net>
2024-08-09 12:02:03 -0400

    output: -primitives mod

--- src/output.rs
+++ src/output.rs
@@ -1,22 +1,15 @@
 mod impls;
-mod primitives;
 mod type_ids;
 
 use std::io::{Result, Write};
 
 use crate::prelude::*;
-
-use primitives::{MatchPrimitives, OutputPrimitives};
 use type_ids::TypeID;
 
 const FORMAT_VERSION_MAJOR: u8 = 0;
 const FORMAT_VERSION_MINOR: u8 = 1;
 const FORMAT_VERSION_PATCH: u8 = 0;
-
-type OutputUInt = <MatchPrimitives<Float> as OutputPrimitives>::UInt;
-type OutputInt = <MatchPrimitives<Float> as OutputPrimitives>::Int;
-type OutputFloat = <MatchPrimitives<Float> as OutputPrimitives>::Float;
-const BYTES_PER_WORD: u8 = <MatchPrimitives<Float> as OutputPrimitives>::BYTES_PER_WORD;
+const BYTES_PER_WORD: u8 = std::mem::size_of::<Float>() as u8;
 
 const PRELUDE: [u8; 4] = [FORMAT_VERSION_MAJOR, FORMAT_VERSION_MINOR, FORMAT_VERSION_PATCH, BYTES_PER_WORD];
 
