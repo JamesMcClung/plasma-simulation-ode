f2fb0c9a3babd7dcc501b372ffdacfe50229812c
James McClung <james@themcclungs.net>
2024-08-12 16:13:01 -0400

    file_writer: +write, write_type_id

--- src/output/file_writer.rs
+++ src/output/file_writer.rs
@@ -15,6 +15,18 @@ impl<F: Write, const BYTES_PER_WORD: u8> FileWriter<F, BYTES_PER_WORD> {
         self.file.write(&[FORMAT_VERSION_MAJOR, FORMAT_VERSION_MINOR, FORMAT_VERSION_PATCH, BYTES_PER_WORD])
     }
 
+    pub fn write<T: TypeID>(&mut self, item: &T) -> Result<usize>
+    where
+        BufWriter<F>: WriteBytes<T>,
+    {
+        self.write_type_id::<T>()?;
+        self.write_bytes(item)
+    }
+
+    pub fn write_type_id<T: TypeID>(&mut self) -> Result<usize> {
+        self.file.write_bytes::<BYTES_PER_WORD>(&T::ID)
+    }
+
     pub fn write_bytes<T: TypeID>(&mut self, item: &T) -> Result<usize>
     where
         BufWriter<F>: WriteBytes<T>,
@@ -48,5 +60,24 @@ mod tests {
         writer.write_prelude().unwrap();
         writer.flush().unwrap();
         assert_eq!(writer.file.get_ref(), &[FORMAT_VERSION_MAJOR, FORMAT_VERSION_MINOR, FORMAT_VERSION_PATCH, 4]);
+    }
+
+    #[rustfmt::skip]
+    #[test]
+    fn write_stuff_4_bytes() {
+        let file = BufWriter::new(Vec::new());
+        let mut writer: FileWriter<_, 4> = FileWriter { file };
+        writer.write(&1.25_f64).unwrap();
+        writer.write(&1.25_f32).unwrap();
+        writer.write(&12_u32).unwrap();
+        writer.flush().unwrap();
+        assert_eq!(writer.file.get_ref(), &[
+            f64::ID, 0, 0   , 0   , // id of first float
+            0      , 0, 0xa0, 0x3f, // value of first float
+            f32::ID, 0, 0   , 0   , // id of second float
+            0      , 0, 0xa0, 0x3f, // value of second float
+            u32::ID, 0, 0   , 0   , // id of uint
+            0x0c   , 0, 0   , 0   , // value of uint
+        ]);
     }
 }
